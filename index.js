ba67165bc726480f2e0130f1c1097240479d919c
Leodias2204 <leonardodmonteiro@hotmail.com>
2023-02-15 21:41:46 -0300

    Add the first page

--- index.js
+++ index.js
@@ -2,20 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { addListener } = require('nodemon');
 const app = express();
-const cors = require("cors")
-const authMiddleware = require('./auth-middleware');
-const firebase = require('./firebase/index');
 
 
-app.use(cors())
 
-const users = [
-    { id: 1, name: "Leonardo Dias",email: "leonardodmonteiro@hotmail.com"},
-    { id: 2, name: "Leandro Paiva", email: "leandropaiva73@gmail.com"},
-    { id: 3, name: "Joel Nunes", email: "joelnunes@gmail.com"},
-]
-
-app.use("/", authMiddleware)
 
 app.get("/users", (request,res) => {
     return res.send({ users })
@@ -30,6 +19,14 @@ app.use(bodyParser.json())
 
 app.get("/login", (req,res) => {
     res.render("login")
+})
+
+app.get("/", (req,res) => {
+    res.render("index")
+    const login = req.body.login
+    const newuser = req.body.newuser
+
+    
 })
 
 
